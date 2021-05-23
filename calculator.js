1d0b08e65e98f4bb092210af1183c5260559ddb3
umitishere <umitsancakli98@gmail.com>
2021-05-24 00:32:32 +0300

    Add two numbers and send response from html file

---
+++ calculator.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+
+    let result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running at port 3000");
+});
